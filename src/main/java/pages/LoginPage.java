bb15e606d0f2282ca24a4ad0986cca88216ebca0
Inikolay <Ibraun12345@gmail.com>
2026-02-09 16:36:59 +0200

    add files

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,7 @@
 package pages;
 
 public class LoginPage {
+    private final String userNameInput = "#user-name";
+    private final String userPasswordInput = "#password";
+    private final String loginButton = "#login-button";
 }
--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,4 +1,15 @@
 package pages;
 
+import com.microsoft.playwright.Page;
+import jdk.internal.org.xml.sax.Locator;
+
 public class MainPage {
+
+    Page page;
+
+    private final String itemsBlock = "#inventory_container";
+
+    public Locator buyButton(String id) {
+        return (Locator) page.locator("#" + id);
+    }
 }
